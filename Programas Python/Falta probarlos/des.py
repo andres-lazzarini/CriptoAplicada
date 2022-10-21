61f57ba948a18017855731c28618418025f03cb8
AndresLazzarini <andres.lazzarini@urvenue.com>
2022-10-21 13:22:07 -0500

    Update DES

--- Programas Python/Falta probarlos/des.py
+++ Programas Python/Falta probarlos/des.py
@@ -30,6 +30,42 @@ def cifra_des(texto):
     l = np.array(lefto, dtype='b').tobytes().decode("ascii")
     r = np.array(righto, dtype='b').tobytes().decode("ascii")
 
-    print(l,r)
+    cifrado = l + r
+    return cifrado
 
-cifra_des(texto)
+def descifra_des(texto):
+
+    splitat = 4
+    left, right = texto[:splitat], texto[splitat:]
+
+    # print(left)
+    # print(right)
+
+    k = ['hola', 'pelo', 'caca', 'pana', 'pena', 'milf', 'love', 'gege', 'jaja', 'yolo', 'papa', 'mama', 'bebe', 'baby', 'palo', 'pala']
+
+    righto = np.frombuffer(bytes(right, 'ascii'), dtype=np.uint8)
+    lefto = np.frombuffer(bytes(left, 'ascii'), dtype=np.uint8)
+
+    # print(lefto, righto)
+
+    for i in range(15, -1, -1):
+
+        ko = np.frombuffer(bytes(k[i], 'ascii'), dtype=np.uint8)
+        temp = ko ^ lefto
+        leftTemp = lefto
+        lefto = temp ^ righto
+        righto = leftTemp
+
+    # print(lefto, righto)
+
+    l = np.array(lefto, dtype='b').tobytes().decode("ascii")
+    r = np.array(righto, dtype='b').tobytes().decode("ascii")
+
+    descifrado = l + r
+    return descifrado
+
+cifrado = cifra_des(texto)
+print(cifrado)
+
+descifrado = descifra_des(cifrado)
+print(descifrado)
